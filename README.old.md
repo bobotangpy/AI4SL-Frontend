27d93a7ba5413aff14738bfea3ccc0243114eff5
Bobo Tang <bobotangpy@gmail.com>
2023-12-13 21:08:13 +0000

    updated Readme &  Tableau resizing

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# AI4SL-Frontend
--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           src="https://wax-butterfly-46f.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fc0d8a943-6d63-4799-9dc3-5128375158ca%2F77132244-2bc1-4b0c-aeb4-403727f10955%2FAI_for_Life_Sciences_icon_logo.png?table=block&id=b8dd7d8e-2c9e-4d94-a111-086043d1bc9d&spaceId=c0d8a943-6d63-4799-9dc3-5128375158ca&width=250&userId=&cache=v2"
           alt="logo"
         />
-        <h1 style={{ width: "fit-content" }}>AI4LS</h1>
+        <h1 style={{ width: "fit-content" }}>AI for Life Science</h1>
       </div>
       <div className="row">
         <Tableau />
